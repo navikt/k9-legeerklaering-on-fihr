bc53497cc93f8e8f45d0defe28f9c988634664e0
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2023-06-29 15:09:49 +0200

    Erstatter next-auth med FHIR client.

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,53 +1,35 @@
 "use client";
 
-import { signIn, signOut, useSession } from "next-auth/react"
-import { Button, Dropdown, InternalHeader } from '@navikt/ds-react';
-import Link from 'next/link';
+import { Dropdown, InternalHeader } from '@navikt/ds-react';
+import React, { useState } from 'react';
+import Client from 'fhirclient/lib/Client';
+import { oauth2 as SMART } from 'fhirclient';
+import { fhirclient } from 'fhirclient/lib/types';
 
 export default function Header() {
-    const {data: session, status} = useSession()
-    const loading = status === "loading"
+    const [client, setClient] = useState<Client>(undefined!);
+
+    React.useEffect(() => {
+        SMART.ready()
+            .then(c => {
+                if (client === undefined) {
+                    setClient(c);
+                    console.log("client", c);
+                }
+            })
+    }, [])
 
     return (
         <header>
             <InternalHeader className="flex justify-between">
                 <InternalHeader.Title as="h1">Legeerklæring - pleiepenger sykt barn</InternalHeader.Title>
-                {!session && (
-                    <>
-                        <Link
-                            href={`/api/auth/signin`}
-                            passHref
-                            className="ml-auto"
-                            onClick={(e) => {
-                                e.preventDefault()
-                                signIn()
-                                    .then((r) => console.log(r))
-                                    .catch((err) => console.log(err))
-                            }}
-                        >
-                            <Button>Sign in</Button>
-                        </Link>
-                    </>
-                )}
-                {session?.user && (
+                {client?.getUserId() && (
                     <Dropdown>
                         <InternalHeader.UserButton
                             as={Dropdown.Toggle}
-                            name={session.user.email ?? session.user.name ?? "Ukjent"}
+                            name={client.getUserId() ?? "Ukjent"}
                             className="ml-auto"
                         />
-                        <Dropdown.Menu>
-                            <Dropdown.Menu.List>
-                                <Dropdown.Menu.List.Item>
-                                    <Link
-                                        href={`/api/auth/signout`}
-                                        onClick={(e) => {
-                                            e.preventDefault()
-                                            signOut()
-                                        }}>Logg ut</Link>
-                                </Dropdown.Menu.List.Item>
-                            </Dropdown.Menu.List>
-                        </Dropdown.Menu>
                     </Dropdown>
                 )}
             </InternalHeader>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
-
 import './globals.css'
 import { Inter } from 'next/font/google'
-import Header from '@/app/components/Header';
-import NextAuthSessionProvider from '@/app/providers/sessionProvider';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,8 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <NextAuthSessionProvider>{children}</NextAuthSessionProvider>
+      <body className={inter.className}>{children}
       </body>
     </html>
   )
