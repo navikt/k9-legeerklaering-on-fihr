c46a0e5add489037054103b708ff08ddb8370c14
Ramin Esfandiari <ramin.esfandiari@nav.no>
2023-07-03 11:17:47 +0200

    Sikrer applikasjonen med FHIRClient (#11)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone"
+    output: "standalone",
+    serverRuntimeConfig: {
+        fhirClientIssuer: process.env.FHIR_CLIENT_ISSUER,
+        fhirClientScope: process.env.FHIR_CLIENT_SCOPE,
+        fhirClientId: process.env.FHIR_CLIENT_ID
+    }
 }
 
 module.exports = nextConfig
