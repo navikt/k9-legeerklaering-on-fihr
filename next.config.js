33bbe922325e7b01a47e131ccd250f30b55022a4
Jostein Stuhaug <jostein.stuhaug@nav.no>
2023-07-11 09:41:57 +0200

    Aktiver eksperimentell next js typed routing.
    Så vi kan ha compile time sjekk av interne ruting variabler.

--- next.config.js
+++ next.config.js
@@ -5,7 +5,10 @@ const nextConfig = {
         fhirClientIssuer: process.env.FHIR_CLIENT_ISSUER,
         fhirClientScope: process.env.FHIR_CLIENT_SCOPE,
         fhirClientId: process.env.FHIR_CLIENT_ID
-    }
+    },
+    experimental: {
+        typedRoutes: true,
+    },
 }
 
 module.exports = nextConfig
