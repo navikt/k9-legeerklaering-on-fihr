6bb1a919b72214bc517181a33f190f946cb95e3b
Jostein Stuhaug <jostein.stuhaug@nav.no>
2023-07-25 15:01:58 +0200

    launchsimulator side for utvikling/test lagt til.
    Simulerer opning av legeerklæringsskjemaet frå EHR systemet for forskjellige scenario. URLer generert på https://launch.smarthealthit.org/

---
+++ src/app/launchsimulation/page.tsx
@@ -0,0 +1,27 @@
+
+export default function Launchsimulation() {
+    return (
+        <div className="mx-auto mt-2 max-w-4xl p-4">
+            <h1>Launch simulation</h1>
+            <p>Her kan du simulere åpning av legeerklæringsskjema fra EHR systemet (Dips).</p>
+            <p>Velg ulike scenario under.</p>
+            <ol className="mt-4">
+                <li>
+                    <a href="http://localhost:3000/?iss=https%3A%2F%2Flaunch.smarthealthit.org%2Fv%2Fr4%2Ffhir&launch=WzAsIjJlNDY3MjlhLTZlZmUtNGY0Ni1iZTkxLTUwYWMxNmI1NDdjNiIsIjQ1NDM3NzQ1LTMzNmUtNDBmZS1iOGRkLTU3OTBlNzg1OWY2ZSIsIkFVVE8iLDAsMCwwLCIiLCIiLCIiLCIiLCIiLCIiLCIiLDAsMV0">
+                        Dr. Legros - Walton King
+                    </a>
+                </li>
+                <li>
+                    <a href="http://localhost:3000/?iss=https%3A%2F%2Flaunch.smarthealthit.org%2Fv%2Fr4%2Ffhir&launch=WzAsIjhhY2IzZDAyLTQwODEtNGY5YS1iODA1LTgwZmZkMTU4YWE2NiIsIjQ3ZWJlZDkyLTczNWQtNDFiOS04NmY2LTdmZGI1ZjQxYTkwNSIsIkFVVE8iLDAsMCwwLCIiLCIiLCIiLCIiLCIiLCIiLCIiLDAsMV0">
+                        Dr. Osinski - Arron Schamberger
+                    </a>
+                </li>
+                <li>
+                    <a href="http://localhost:3000/?iss=https%3A%2F%2Flaunch.smarthealthit.org%2Fv%2Fr4%2Ffhir&launch=WzAsIjBkNTg1MDk0LTFlMDQtNGM3MS04NzM1LTQ0NGU5OWJmN2E5OSIsIjQ3ZWJlZDkyLTczNWQtNDFiOS04NmY2LTdmZGI1ZjQxYTkwNSIsIkFVVE8iLDAsMCwwLCIiLCIiLCIiLCIiLCIiLCIiLCIiLDAsMV0">
+                        Dr. Osinski - Mao Kilback
+                    </a>
+                </li>
+            </ol>
+        </div>
+    )
+}
