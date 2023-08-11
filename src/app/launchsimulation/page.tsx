8365858883fabbd9bbc78db87130c851931662b1
Jostein Stuhaug <jostein.stuhaug@nav.no>
2023-08-11 11:01:22 +0200

    Fjerna host spesifikasjon frå launchsimulation linker.
    launchsimulation skal no fungere både på localhost og i testmiljø.

--- src/app/launchsimulation/page.tsx
+++ src/app/launchsimulation/page.tsx
@@ -7,17 +7,17 @@ export default function Launchsimulation() {
             <p>Velg ulike scenario under.</p>
             <ol className="mt-4">
                 <li>
-                    <a href="http://localhost:3000/?iss=https%3A%2F%2Flaunch.smarthealthit.org%2Fv%2Fr4%2Ffhir&launch=WzAsIjJlNDY3MjlhLTZlZmUtNGY0Ni1iZTkxLTUwYWMxNmI1NDdjNiIsIjQ1NDM3NzQ1LTMzNmUtNDBmZS1iOGRkLTU3OTBlNzg1OWY2ZSIsIkFVVE8iLDAsMCwwLCIiLCIiLCIiLCIiLCIiLCIiLCIiLDAsMV0">
+                    <a href="/?iss=https%3A%2F%2Flaunch.smarthealthit.org%2Fv%2Fr4%2Ffhir&launch=WzAsIjJlNDY3MjlhLTZlZmUtNGY0Ni1iZTkxLTUwYWMxNmI1NDdjNiIsIjQ1NDM3NzQ1LTMzNmUtNDBmZS1iOGRkLTU3OTBlNzg1OWY2ZSIsIkFVVE8iLDAsMCwwLCIiLCIiLCIiLCIiLCIiLCIiLCIiLDAsMV0">
                         Dr. Legros - Walton King
                     </a>
                 </li>
                 <li>
-                    <a href="http://localhost:3000/?iss=https%3A%2F%2Flaunch.smarthealthit.org%2Fv%2Fr4%2Ffhir&launch=WzAsIjhhY2IzZDAyLTQwODEtNGY5YS1iODA1LTgwZmZkMTU4YWE2NiIsIjQ3ZWJlZDkyLTczNWQtNDFiOS04NmY2LTdmZGI1ZjQxYTkwNSIsIkFVVE8iLDAsMCwwLCIiLCIiLCIiLCIiLCIiLCIiLCIiLDAsMV0">
+                    <a href="/?iss=https%3A%2F%2Flaunch.smarthealthit.org%2Fv%2Fr4%2Ffhir&launch=WzAsIjhhY2IzZDAyLTQwODEtNGY5YS1iODA1LTgwZmZkMTU4YWE2NiIsIjQ3ZWJlZDkyLTczNWQtNDFiOS04NmY2LTdmZGI1ZjQxYTkwNSIsIkFVVE8iLDAsMCwwLCIiLCIiLCIiLCIiLCIiLCIiLCIiLDAsMV0">
                         Dr. Osinski - Arron Schamberger
                     </a>
                 </li>
                 <li>
-                    <a href="http://localhost:3000/?iss=https%3A%2F%2Flaunch.smarthealthit.org%2Fv%2Fr4%2Ffhir&launch=WzAsIjBkNTg1MDk0LTFlMDQtNGM3MS04NzM1LTQ0NGU5OWJmN2E5OSIsIjQ3ZWJlZDkyLTczNWQtNDFiOS04NmY2LTdmZGI1ZjQxYTkwNSIsIkFVVE8iLDAsMCwwLCIiLCIiLCIiLCIiLCIiLCIiLCIiLDAsMV0">
+                    <a href="/?iss=https%3A%2F%2Flaunch.smarthealthit.org%2Fv%2Fr4%2Ffhir&launch=WzAsIjBkNTg1MDk0LTFlMDQtNGM3MS04NzM1LTQ0NGU5OWJmN2E5OSIsIjQ3ZWJlZDkyLTczNWQtNDFiOS04NmY2LTdmZGI1ZjQxYTkwNSIsIkFVVE8iLDAsMCwwLCIiLCIiLCIiLCIiLCIiLCIiLCIiLDAsMV0">
                         Dr. Osinski - Mao Kilback
                     </a>
                 </li>
