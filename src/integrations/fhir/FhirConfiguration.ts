74f362f84c162f5ec4ca52cfb507d44a9eb3e0ef
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2023-09-18 15:34:02 +0200

    Piper lokal logging gjennom pino-pretty.

--- src/integrations/fhir/FhirConfiguration.ts
+++ src/integrations/fhir/FhirConfiguration.ts
@@ -1,3 +1,4 @@
+import { logger } from '@/utils/logger';
 
 export class FhirConfiguration {
     private _fhirbaseurl: string;
@@ -14,12 +15,16 @@ export class FhirConfiguration {
     constructor() {
         const {FHIR_BASE_URL, FHIR_SUBSCRIPTION_KEY} = process.env;
         if (!FHIR_BASE_URL) {
-            throw new Error("FHIR_BASE_URL is not defined.");
+            const error = new Error("FHIR_BASE_URL is not defined.");
+            logger.error(error);
+            throw error;
         }
         this._fhirbaseurl = FHIR_BASE_URL;
 
         if (!FHIR_SUBSCRIPTION_KEY) {
-            throw new Error("FHIR_SUBSCRIPTION_KEY is not defined.");
+            const error1 = new Error("FHIR_SUBSCRIPTION_KEY is not defined.");
+            logger.error(error1);
+            throw error1;
         }
         this._fhirsubscriptionkey = FHIR_SUBSCRIPTION_KEY;
     }
