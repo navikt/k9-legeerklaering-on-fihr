df08be3142e398272adecbaf1bb90ad146daae85
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2023-09-18 11:39:47 +0200

    Logger req/res på i middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,10 +4,13 @@ import { NextRequest, NextResponse } from 'next/server';
 export const FHIR_AUTHORIZATION_TOKEN = "fhir-authorization-token";
 export const middleware = (request: NextRequest): NextResponse => {
     const {method, nextUrl} = request
-    console.log(`Request ${method} ${nextUrl.pathname}`)
+    // Replace the IDs in the pathname for Patient and Practitioner
+    const maskedPathname = nextUrl.pathname.replace(/(\/api\/fhir\/(Patient|Practitioner)\/)[^/]+/, '$1<masked>');
+    console.log(`Request ${method} ${maskedPathname}`)
+
     const authorization = request.headers.get(FHIR_AUTHORIZATION_TOKEN);
     if (!authorization) {
-        return new NextResponse(
+        const unauthorizedResponse = new NextResponse(
             JSON.stringify({
                 message: 'authentication failed'
             }),
@@ -15,12 +18,14 @@ export const middleware = (request: NextRequest): NextResponse => {
                 status: 401,
                 statusText: 'Unauthorized',
             }
-        )
+        );
+        console.log(`Response ${unauthorizedResponse.status} ${unauthorizedResponse.statusText} ${maskedPathname}`)
+        return unauthorizedResponse
     }
     // TODO: Validere token issuer?
     const response = NextResponse.next();
     const {status, statusText} = response
-    console.log(`Response ${status} ${statusText} ${nextUrl.pathname}`)
+    console.log(`Response ${status} ${statusText} ${maskedPathname}`)
     return response
 };
 
