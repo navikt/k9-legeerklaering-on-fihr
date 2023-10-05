af814d5076397b416b7492ab3f66303c82cfeeff
Jostein Stuhaug <jostein.stuhaug@nav.no>
2023-10-05 14:20:43 +0200

    Improved response validation in getCurrentUser route handler.
    Now we will get a error thrown if the request from this handler to the fhir api fails.
    
    Also removes use of the fhirclient for typing. Using @ahryman40k/ts-fhir-types instead.

--- src/app/api/fhir/PractitionerRole/getCurrentUser/route.ts
+++ src/app/api/fhir/PractitionerRole/getCurrentUser/route.ts
@@ -1,5 +1,4 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { fhirclient } from 'fhirclient/lib/types';
 import { R4 } from '@ahryman40k/ts-fhir-types';
 import { IPractitionerRole } from '@ahryman40k/ts-fhir-types/lib/R4';
 import { validateOrThrow } from '@/integrations/fhir/fhirValidator';
@@ -7,7 +6,13 @@ import { headers } from 'next/headers';
 import { FHIR_AUTHORIZATION_TOKEN } from '@/middleware';
 import { logRequest, logResponse } from '@/utils/loggerUtils';
 import { getServerEnv } from '@/utils/env';
-import Bundle = fhirclient.FHIR.Bundle;
+
+const isPractitionerRoleOrThrow = (resource?: R4.IResourceList): R4.IPractitionerRole => {
+    if(R4.RTTI_PractitionerRole.is(resource)) {
+        return resource;
+    }
+    throw new Error(`resource ${resource?.resourceType} is not a ${R4.RTTI_PractitionerRole.name}`);
+}
 
 export const GET = async (request: NextRequest): Promise<NextResponse<IPractitionerRole | Error>> => {
     logRequest(request)
@@ -21,13 +26,18 @@ export const GET = async (request: NextRequest): Promise<NextResponse<IPractitio
         }
     });
 
-    const bundle: Bundle = await response.json();
-    if (!bundle.entry || bundle.entry.length === 0) {
-        throw new Error("No entries found in the bundle.");
-    }
+    if(response.ok) {
+        const bundle = validateOrThrow(R4.RTTI_Bundle.decode(await response.json()));
+        if (!bundle.entry || bundle.entry.length === 0) {
+            throw new Error("No entries found in the bundle.");
+        }
 
-    const practitionerRole = validateOrThrow(R4.RTTI_PractitionerRole.decode(bundle.entry[0].resource));
-    const nextResponse = NextResponse.json(practitionerRole);
-    logResponse(request.nextUrl, nextResponse)
-    return nextResponse
+        const practitionerRole: R4.IPractitionerRole = isPractitionerRoleOrThrow(bundle.entry[0].resource)
+        const nextResponse = NextResponse.json(practitionerRole);
+        logResponse(request.nextUrl, nextResponse)
+        return nextResponse
+    } else {
+        const text = await response.text()
+        throw new Error(`invalid response to fhir fetch: ${response.status} - ${response.statusText} == ${text}`)
+    }
 }
