44cc1cb85b79fe18cd35c0767b00c598b6ad703b
Jostein Stuhaug <jostein.stuhaug@nav.no>
2023-10-13 14:37:33 +0200

    Separated Page component out to enable simulating it with fake api implementations.

--- src/app/components/legeerklaering/LoadingIndicator.tsx
+++ src/app/components/legeerklaering/LoadingIndicator.tsx
@@ -2,11 +2,17 @@ import {Loader} from "@navikt/ds-react";
 
 import css from './legeerklaering.module.css'
 
-export default function LoadingIndicator() {
+export interface LoadingIndicatorProps {
+    readonly txt?: string;
+}
+
+const LoadingIndicator = ({txt = "Laster EHR informasjon"}: LoadingIndicatorProps) => {
     return (
         <div className={css.loadingContainer}>
             <Loader size="3xlarge" />
-            <div role="alert">Laster EHR informasjon</div>
+            <div role="alert">{ txt }</div>
         </div>
     )
 }
+
+export default LoadingIndicator
