37a735ef14a721078c316ae233210d57ade6f279
Jostein Stuhaug <jostein.stuhaug@nav.no>
2023-10-16 14:33:07 +0200

    Redirect /launchsimulation to /simulation.
    Easier path to type

--- src/app/launchsimulation/page.tsx
+++ src/app/launchsimulation/page.tsx
@@ -1,45 +1,6 @@
-import { isSimulationAllowed } from "@/utils/environment";
+import { redirect } from "next/navigation";
 
-// Force the page to be dynamically rendered (not static pregenation during build)
-// This is neccessary so that the isSimulationAllowed() function can read the SIMULATION_ALLOWED env variable at runtime
-// instead of at build time.
-export const dynamic = 'force-dynamic';
-
-const SimulationNotAllowed = () => {
-    return (
-        <>
-            <h2>Not allowed</h2>
-            <p>This deployment does not allow simulation of backends. This page is only meant to be used during development/testing.</p>
-            <p>
-                <a href="/">Back to start page.</a>
-            </p>
-        </>
-    )
-}
-
+// This page has moved
 export default function Launchsimulation() {
-    const allowed = isSimulationAllowed()
-    return allowed ? (
-        <>
-            <p>Her kan du simulere åpning av legeerklæringsskjema fra EHR systemet (Dips).</p>
-            <p>Velg pasient.</p>
-            <ol className="mt-4">
-                <li>
-                    <a href="/?iss=https://api.dips.no/fhir&launch=cdp1000807&simulation=true">
-                        Roland Gundersen
-                    </a>
-                </li>
-                <li>
-                    <a href="/?iss=https://api.dips.no/fhir&launch=cdp1000813&simulation=true">
-                        Line Danser
-                    </a>
-                </li>
-                <li>
-                    <a href="/?iss=https://api.dips.no/fhir&launch=cdp2008909&simulation=true">
-                        Gul Penn
-                    </a>
-                </li>
-            </ol>
-        </>
-    ): SimulationNotAllowed()
+    redirect("/simulation")
 }
