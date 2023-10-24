8d287f7f3f47bab2c9489d50a0df4fa401cd8320
Jostein Stuhaug <jostein.stuhaug@nav.no>
2023-10-24 16:28:30 +0200

    Omsorgspersoner valg og registrering på plass i nytt skjema alternativ.
    Avkrysningsliste med personer tilknytta pasient henta frå fhir api visast i det alternative skjemaet, og avkryssa personer sin ehrId blir med i submit data.

--- src/app/simulation/portalpoc/legeerklaering-pleiepenger-sykt-barn/new/page.tsx
+++ src/app/simulation/portalpoc/legeerklaering-pleiepenger-sykt-barn/new/page.tsx
@@ -23,7 +23,6 @@ import HoveddiagnoseSelect from "@/app/components/diagnosekoder/HoveddiagnoseSel
 import BidiagnoseSelect from "@/app/components/diagnosekoder/BidiagnoseSelect";
 import MultiDatePeriodInput from "@/app/components/multidateperiod/MultiDatePeriodInput";
 import { ObjectSchema } from "yup";
-import LegeerklaeringData from "@/app/components/legeerklaering/LegeerklaeringData";
 import * as yup from "yup";
 import { Diagnosekode } from "@navikt/diagnosekoder";
 import DatePeriod from "@/models/DatePeriod";
@@ -36,6 +35,7 @@ interface FormData {
     bidiagnoser: Diagnosekode[];
     tilsynPerioder: DatePeriod[];
     innleggelsesPerioder: DatePeriod[];
+    omsorgspersoner: string[];
 }
 
 const diagnosekodeValidation: ObjectSchema<Diagnosekode> = yup.object({
@@ -62,13 +62,13 @@ const schema: ObjectSchema<FormData> = yup.object({
     bidiagnoser: yup.array().of(diagnosekodeValidation).required(),
     legensVurdering: yup.string().trim().required(tekst("legeerklaering.legens-vurdering.paakrevd")),
     tilsynPerioder: yup.array().of(datePeriodValidation).min(1, ({min}) => `Minimum ${min} periode må spesifiseres`).required(),
-    innleggelsesPerioder: yup.array().of(datePeriodValidation).required()
+    innleggelsesPerioder: yup.array().of(datePeriodValidation).required(),
+    omsorgspersoner: yup.array().of(yup.string().trim().required()).min(0, "Minst en omsorgsperson er påkrevd").required()
 })
 
 
 const Page = () => {
     const api = useContext(BaseApiContext)
-    const [submittedData, setSubmittedData] = useState<FormData | null>(null);
 
     const {
         control,
@@ -86,16 +86,16 @@ const Page = () => {
                 start: undefined,
                 end: undefined,
             }],
-            innleggelsesPerioder: []
+            innleggelsesPerioder: [],
+            omsorgspersoner: []
         }
     })
 
-    const onSubmit = (data: LegeerklaeringData) => {
-        setSubmittedData(data)
-        logger.info("Form data", data);
+    const onSubmit = (data: FormData) => {
+        logger.info("TODO Submit form data", data);
     };
 
-    const onError: SubmitErrorHandler<LegeerklaeringData> = errors => {
+    const onError: SubmitErrorHandler<FormData> = errors => {
         logger.warn("form validation errors", errors)
     }
 
@@ -103,35 +103,42 @@ const Page = () => {
     return <CenterColumn>
         <InitDataDependentRender baseApi={api} render={(initData) => {
             return <>
+            <form onSubmit={handleSubmit(onSubmit, onError)}>
                 <PaddedPanel>
                     <Heading spacing level="2" size="medium">Om barnet </Heading>
-                    <Label>{initData.patient.name}</Label> - Fødselsdato {initData.patient.birthDate?.toLocaleDateString()} <small>({initData.patient.identifier})</small>
-
-
+                    <Label>{initData.patient.name}</Label> - Fødselsdato {initData.patient.birthDate?.toLocaleDateString()} <small>({initData.patient.fnr})</small>
                 </PaddedPanel>
                 <PaddedPanel>
-                    <CheckboxGroup
-                        legend={<Heading level="2" size="medium">Omsorgspersoner</Heading>}
-                        // description="De personer som blir valgt her får tilgang til å lese og bruke legeerklæringen"
-                        description={
-                            <ReadMore size="small" header="De personer som blir valgt her får tilgang til å lese og bruke legeerklæringen">
-                                <p>
-                                Dette tilsvarer at du tidligere ville skrevet ut og levert legeerklæringen på papir til disse personene,
-                                slik at de kunne søke om pleiepenger.
-                                </p>
-                                <p>Det er derfor viktig at du tenker på personvern og kontrollerer
-                                at disse faktisk skal ha tilgang til informasjonen i legeerklæringen.
-                                </p>
-                                <p>
-                                    Hvis den/de som skal bruke legeerklæringen ikke viser her må du få lagt de inn som relaterte personer
-                                    på pasienten i ditt journalsystem, og oppfriske.
-                                </p>
-                            </ReadMore>
-                        }
-                    >
-                        <Checkbox value="12314254">Nille Lurius <small>(19900204444221)</small></Checkbox>
-                        <Checkbox value="4242442">Lars Loss <small>(19920306341271)</small></Checkbox>
-                    </CheckboxGroup>
+                    <Controller name="omsorgspersoner" control={control} render={({field: {onChange, value}}) => (
+                        <CheckboxGroup
+                            legend={<Heading level="2" size="medium">Omsorgspersoner</Heading>}
+                            value={value}
+                            onChange={onChange}
+                            error={errors.omsorgspersoner?.message}
+                            // description="De personer som blir valgt her får tilgang til å lese og bruke legeerklæringen"
+                            description={
+                                <ReadMore size="small" header="De personer som blir valgt her får tilgang til å lese og bruke legeerklæringen">
+                                    <p>
+                                        Dette tilsvarer at du tidligere ville skrevet ut og levert legeerklæringen på papir til disse personene,
+                                        slik at de kunne søke om pleiepenger.
+                                    </p>
+                                    <p>Det er derfor viktig at du tenker på personvern og kontrollerer
+                                        at disse faktisk skal ha tilgang til informasjonen i legeerklæringen.
+                                    </p>
+                                    <p>
+                                        Hvis den/de som skal bruke legeerklæringen ikke viser her må du få lagt de inn som relaterte personer
+                                        på pasienten i ditt journalsystem, og oppfriske.
+                                    </p>
+                                </ReadMore>
+                            }
+                        >
+                            {
+                                api.initData?.patient.caretakers.map(relatedPerson => (
+                                    <Checkbox key={`rp${relatedPerson.ehrId}`} value={relatedPerson.ehrId} >{relatedPerson.name} <small>({relatedPerson.fnr})</small></Checkbox>
+                                ))
+                            }
+                        </CheckboxGroup>
+                    )} />
                 </PaddedPanel>
                 <PaddedPanel>
                     <Heading spacing level="2" size="medium">{tekst("legeerklaering.legens-vurdering.tittel")}</Heading>
@@ -143,6 +150,8 @@ const Page = () => {
                     </ReadMore>
                     <Textarea
                         label={tekst("legeerklaering.legens-vurdering.label")}
+                        {...register("legensVurdering", {required: true})}
+                        error={errors.legensVurdering?.message }
                         minRows={10}
                     />
                 </PaddedPanel>
@@ -203,11 +212,12 @@ const Page = () => {
 
                 <Box padding="4">
                 <HStack gap="4">
-                    <Button variant="secondary">Lagre</Button>
+                    <Button variant="secondary" type="submit">Lagre</Button>
                     <Button variant="secondary">Godkjenn</Button>
                     <Button disabled>Send til NAV</Button>
                 </HStack>
                 </Box>
+            </form>
             </>
         }} />
 
