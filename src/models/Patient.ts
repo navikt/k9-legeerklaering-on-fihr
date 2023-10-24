342f90249c7ddfde7c184c2dcd51c45611974ec4
Jostein Stuhaug <jostein.stuhaug@nav.no>
2023-10-24 11:34:03 +0200

    Added caretakers (RelatedPersons) to patient (InitData).
    This also includes improvements to resolving of fødselsnr or d-nummer identifiers, and some other related stuff.

--- src/models/Patient.ts
+++ src/models/Patient.ts
@@ -1,5 +1,8 @@
+import RelatedPerson from "@/models/RelatedPerson";
+
 export default interface Patient {
     readonly name: string;
     readonly identifier: string;
     readonly birthDate?: Date;
+    readonly caretakers: RelatedPerson[];
 }
