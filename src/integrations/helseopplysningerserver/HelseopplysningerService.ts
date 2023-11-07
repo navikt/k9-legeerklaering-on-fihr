d3ea970b31f88bebd8c23a6154dc3898a6893dc4
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2023-11-07 09:12:33 +0100

    Bruker access_token.

--- src/integrations/helseopplysningerserver/HelseopplysningerService.ts
+++ src/integrations/helseopplysningerserver/HelseopplysningerService.ts
@@ -15,58 +15,59 @@ export default class HelseopplysningerService {
         logger.info("Genererer PDF...");
         try {
             const tokenSet = await AzureClientConfiguration.getServerHelseToken();
+            const body = JSON.stringify({
+                legeerklæring: {
+                    pasient: {
+                        navn: {
+                            fornavn: data.barn.name,
+                            etternavn: data.barn.name,
+                        },
+                        id: data.barn.fnr,
+                        fødselsdato: data.barn.birthDate,
+                    },
+                    vurdering: data.legensVurdering,
+                    hoveddiagnose: {
+                        term: data.hoveddiagnose?.text,
+                        kode: data.hoveddiagnose?.code,
+                    },
+                    bidiagnoser: data.bidiagnoser.map(bidiagnose => ({
+                        term: bidiagnose.text,
+                        kode: bidiagnose.code,
+                    })),
+                    tilsynsPerioder: data.tilsynPerioder.map(tilsyn => ({
+                        fom: tilsyn.start,
+                        tom: tilsyn.end,
+                    })),
+                    innleggelsesPerioder: data.innleggelsesPerioder.map(innleggelse => ({
+                        fom: innleggelse.start,
+                        tom: innleggelse.end,
+                    }))
+                },
+                lege: {
+                    navn: {
+                        fornavn: data.lege.name,
+                        etternavn: data.lege.name,
+                    },
+                    hpr: data.lege.hprNumber,
+                },
+                sykehus: {
+                    navn: data.sykehus.name,
+                    telefonnummer: data.sykehus.phoneNumber,
+                    adresse: {
+                        gateadresse: data.sykehus.address?.line1,
+                        gateadresse2: data.sykehus.address?.line2,
+                        postkode: data.sykehus.address?.postalCode,
+                        by: data.sykehus.address?.city,
+                    }
+                }
+            });
             const pdfResponse = await fetch(`${this.baseUrl}/pdf/`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
-                    "Authorization": `Bearer ${tokenSet.id_token}`
+                    "Authorization": `Bearer ${tokenSet.access_token}`
                 },
-                body: JSON.stringify({
-                    legeerklæring: {
-                        pasient: {
-                            navn: {
-                                fornavn: data.barn.name,
-                                etternavn: data.barn.name,
-                            },
-                            id: data.barn.fnr,
-                            fødselsdato: data.barn.birthDate,
-                        },
-                        vurdering: data.legensVurdering,
-                        hoveddiagnose: {
-                            term: data.hoveddiagnose?.text,
-                            kode: data.hoveddiagnose?.code,
-                        },
-                        bidiagnoser: data.bidiagnoser.map(bidiagnose => ({
-                            term: bidiagnose.text,
-                            kode: bidiagnose.code,
-                        })),
-                        tilsynsPerioder: data.tilsynPerioder.map(tilsyn => ({
-                            fom: tilsyn.start,
-                            tom: tilsyn.end,
-                        })),
-                        innleggelsesPerioder: data.innleggelsesPerioder.map(innleggelse => ({
-                            fom: innleggelse.start,
-                            tom: innleggelse.end,
-                        }))
-                    },
-                    lege: {
-                        navn: {
-                            fornavn: data.lege.name,
-                            etternavn: data.lege.name,
-                        },
-                        hpr: data.lege.hprNumber,
-                    },
-                    sykehus: {
-                        navn: data.sykehus.name,
-                        telefonnummer: data.sykehus.phoneNumber,
-                        adresse: {
-                            gateadresse: data.sykehus.address?.line1,
-                            gateadresse2: data.sykehus.address?.line2,
-                            postkode: data.sykehus.address?.postalCode,
-                            by: data.sykehus.address?.city,
-                        }
-                    }
-                })
+                body: body
             })
             if (pdfResponse.ok) return pdfResponse;
             else throw new Error(`Unexpected status code when fetching pdf (${pdfResponse.status} - ${pdfResponse.statusText}).`, {
