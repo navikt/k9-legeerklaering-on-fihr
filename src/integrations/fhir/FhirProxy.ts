c715768d76ecb65e3daea0f97deb018f7df053c9
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2023-11-23 15:15:21 +0100

    Duplex fungerer. Ignorerer TS-warning.

--- src/integrations/fhir/FhirProxy.ts
+++ src/integrations/fhir/FhirProxy.ts
@@ -73,6 +73,7 @@ export class FhirProxy {
                 method: incomingReq.method,
                 headers: newHeaders,
                 body: incomingReq.body,
+                // @ts-ignore
                 duplex: "half",
                 mode: incomingReq.mode,
                 credentials: incomingReq.credentials,
