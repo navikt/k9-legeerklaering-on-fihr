c3191e283d4664d803c1dac31fcb93d6ee015cbb
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2023-11-29 08:32:00 +0100

    Fikser navn på lege og pasient ved innsending.

--- src/app/api/oppsummering/mapper/mapper.ts
+++ src/app/api/oppsummering/mapper/mapper.ts
@@ -1,49 +1,55 @@
 import LegeerklaeringData from '@/app/components/legeerklaering/LegeerklaeringData';
 
-export const mapTilPSBLegeerklæringInnsending = (data: LegeerklaeringData): PSBLegeerklæringInnsending => ({
-    legeerklæring: {
-        pasient: {
-            navn: {
-                fornavn: data.barn.name,
-                etternavn: data.barn.name,
+
+// som navn
+const somNavn = (navn: string): Navn => {
+    const navnArray = navn.split(",");
+    return {
+        fornavn: navnArray[1],
+        etternavn: navnArray[0],
+    };
+};
+
+export const mapTilPSBLegeerklæringInnsending = (data: LegeerklaeringData): PSBLegeerklæringInnsending => {
+    return ({
+        legeerklæring: {
+            pasient: {
+                navn: somNavn(data.barn.name),
+                id: data.barn.fnr,
+                fødselsdato: data.barn.birthDate,
             },
-            id: data.barn.fnr,
-            fødselsdato: data.barn.birthDate,
+            vurdering: data.legensVurdering,
+            vurderingAvOmsorgspersoner: data.vurderingAvOmsorgspersoner,
+            hoveddiagnose: {
+                term: data.hoveddiagnose?.text,
+                kode: data.hoveddiagnose?.code,
+            },
+            bidiagnoser: data.bidiagnoser.map(bidiagnose => ({
+                term: bidiagnose.text,
+                kode: bidiagnose.code,
+            })),
+            tilsynsPerioder: data.tilsynPerioder.map(tilsyn => ({
+                fom: tilsyn.start,
+                tom: tilsyn.end,
+            })),
+            innleggelsesPerioder: data.innleggelsesPerioder.map(innleggelse => ({
+                fom: innleggelse.start,
+                tom: innleggelse.end,
+            }))
         },
-        vurdering: data.legensVurdering,
-        vurderingAvOmsorgspersoner: data.vurderingAvOmsorgspersoner,
-        hoveddiagnose: {
-            term: data.hoveddiagnose?.text,
-            kode: data.hoveddiagnose?.code,
+        lege: {
+            navn: somNavn(data.lege.name),
+            hpr: data.lege.hprNumber,
         },
-        bidiagnoser: data.bidiagnoser.map(bidiagnose => ({
-            term: bidiagnose.text,
-            kode: bidiagnose.code,
-        })),
-        tilsynsPerioder: data.tilsynPerioder.map(tilsyn => ({
-            fom: tilsyn.start,
-            tom: tilsyn.end,
-        })),
-        innleggelsesPerioder: data.innleggelsesPerioder.map(innleggelse => ({
-            fom: innleggelse.start,
-            tom: innleggelse.end,
-        }))
-    },
-    lege: {
-        navn: {
-            fornavn: data.lege.name,
-            etternavn: data.lege.name,
-        },
-        hpr: data.lege.hprNumber,
-    },
-    sykehus: {
-        navn: data.sykehus.name,
-        telefonnummer: data.sykehus.phoneNumber,
-        adresse: {
-            gateadresse: data.sykehus.address?.line1,
-            gateadresse2: data.sykehus.address?.line2,
-            postkode: data.sykehus.address?.postalCode,
-            by: data.sykehus.address?.city,
+        sykehus: {
+            navn: data.sykehus.name,
+            telefonnummer: data.sykehus.phoneNumber,
+            adresse: {
+                gateadresse: data.sykehus.address?.line1,
+                gateadresse2: data.sykehus.address?.line2,
+                postkode: data.sykehus.address?.postalCode,
+                by: data.sykehus.address?.city,
+            }
         }
-    }
-} as PSBLegeerklæringInnsending);
+    } as PSBLegeerklæringInnsending);
+};
