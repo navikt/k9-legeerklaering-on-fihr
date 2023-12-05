6dd9dfe4af2dba681baa3b6b018e4f2d8539064b
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2023-12-05 11:18:19 +0100

    Legger til lukkeknapp for DiagnosekodeSearchModal.

--- src/app/components/diagnosekoder/DiagnosekodeSearchModal.tsx
+++ src/app/components/diagnosekoder/DiagnosekodeSearchModal.tsx
@@ -1,8 +1,7 @@
 import DiagnosekodeSearch, { OnSelectedDiagnose } from "@/app/components/diagnosekoder/DiagnosekodeSearch";
-import { Heading, Modal } from "@navikt/ds-react";
+import { Modal } from "@navikt/ds-react";
 
 import diagnosekoderCss from './diagnosekoder.module.css';
-import { componentSize } from '@/utils/constants';
 
 export interface DiagnosekodeSearchModalProps extends OnSelectedDiagnose {
     readonly open: boolean;
@@ -11,10 +10,18 @@ export interface DiagnosekodeSearchModalProps extends OnSelectedDiagnose {
 
 const DiagnosekodeSearchModal = (props: DiagnosekodeSearchModalProps) => {
     return (
-        <Modal open={props.open} onClose={props.onClose} aria-labelledby="diagnosekode-modal-heading" >
+        <Modal
+            open={props.open}
+            onClose={props.onClose}
+            aria-labelledby="diagnosekode-modal-heading"
+            header={{
+                heading: "Velg diagnosekode",
+                size: "small",
+                closeButton: true,
+            }}
+        >
             <Modal.Body className={diagnosekoderCss.modal}>
-                <Heading size={componentSize} id="diagnosekode-modal-heading" className={diagnosekoderCss.heading}>Velg diagnosekode</Heading>
-                <DiagnosekodeSearch onSelectedDiagnose={props.onSelectedDiagnose} />
+                <DiagnosekodeSearch onSelectedDiagnose={props.onSelectedDiagnose}/>
             </Modal.Body>
         </Modal>
     )
