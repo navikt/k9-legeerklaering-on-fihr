c08f11b328d2bd8bd5d9ea238942341c4ff2aba6
Jostein Stuhaug <jostein.stuhaug@nav.no>
2024-01-22 15:55:12 +0100

    FhirSession validering på generatePdf kall.
    Server handler for generatePdf kallet validerer no at den som kaller har gyldig fhir session for det hpr nummer legeerklæringa er oppgitt til å vere frå.

--- src/app/api/oppsummering/pdf/route.ts
+++ src/app/api/oppsummering/pdf/route.ts
@@ -1,9 +1,11 @@
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { logRequest } from '@/utils/loggerUtils';
 import { logger } from '@navikt/next-logger';
 import { FakeHelseopplysningerApi1 } from "@/app/simulation/fakeHelseopplysningerApi1";
 import HelseopplysningerClient from "@/integrations/helseopplysningerserver/HelseopplysningerClient";
 import { PSBLegeerklæringInnsending } from "@/integrations/helseopplysningerserver/types/HelseopplysningerTypes";
+import { FhirSession } from "@/auth/fhir/FhirSession";
+import { JwtVerificationInput } from "@/auth/fhir/JwtVerificationInput";
 
 const initHelseopplysningerApi = () => process.env.FAKE_HELSEOPPLYSNINGER === "fake1" ?
     new FakeHelseopplysningerApi1() :
@@ -11,11 +13,20 @@ const initHelseopplysningerApi = () => process.env.FAKE_HELSEOPPLYSNINGER === "f
 
 export const POST = async (request: NextRequest): Promise<Response> => {
     logRequest(request);
-    logger.info("Registrerer legeerklæring...");
+    // Check that requestor has a valid fhir session
+    const authHeader = request.headers.get("Authorization") || ""
+    const fhirSession = await FhirSession.fromVerifiedJWT(JwtVerificationInput.fromAuthorizationHeader(authHeader))
+    logger.info("genererer legeerklæring pdf...");
     const innsending = await request.json() as PSBLegeerklæringInnsending;
+    // Check that hpr-number of session equals posted document
+    if(innsending.lege.hpr === fhirSession.hprNumber) {
+        // XXX Her kunne vi evt ha validering av nokre innsendte skjemaopplysninger opp mot fhir api, før registrering av
+        // data i NAV sine system. Feks at lege sitt namn er korrekt, spesialiststatus, pasient-namn, etc.
+        const helseopplysningerApi = initHelseopplysningerApi()
+        const pdfBlob = await helseopplysningerApi.generatePdf(innsending)
+        return new Response(pdfBlob)
 
-    const helseopplysningerApi = initHelseopplysningerApi()
-
-    const pdfBlob = await helseopplysningerApi.generatePdf(innsending)
-    return new Response(pdfBlob)
+    } else {
+        return new NextResponse(`fhir session hpr-nummer ulikt legeerklæring (${fhirSession.hprNumber} != ${innsending.lege.hpr})`, {status: 400})
+    }
 }
