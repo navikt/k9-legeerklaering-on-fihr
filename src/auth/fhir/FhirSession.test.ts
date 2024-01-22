887782a41e722e392f88dcc0342e4c740821019c
Jostein Stuhaug <jostein.stuhaug@nav.no>
2024-01-22 13:46:31 +0100

    FhirSession validerer no at client_id er korrekt.
    For å unngå at nokon kan bruke jwt tokens laga for ein anna app inn mot vår server.

--- src/auth/fhir/FhirSession.test.ts
+++ src/auth/fhir/FhirSession.test.ts
@@ -8,16 +8,19 @@ import { JwtVerificationInput } from "@/auth/fhir/JwtVerificationInput";
 import { FhirSession } from "@/auth/fhir/FhirSession";
 import { FhirAuthError } from "@/auth/fhir/FhirAuthError";
 import { FhirSessionIssuers } from "@/auth/fhir/FhirSessionIssuers";
+import { fhirClientId } from "@/utils/environment";
 
 
-const prepareFakeValidVerificationInput = async () => {
+const prepareFakeValidVerificationInput = async (clientId?: string) => {
     const { publicKey, privateKey } = await jose.generateKeyPair('RS256')
     const issuer = FhirSessionIssuers.OPENDIPS_TEST
     const hprNumber = "111222"
+    const configuredClientId = clientId || await fhirClientId()
     const fakeValidPayload: JWTPayload = {
         aud: "legeerklæring test",
         sub: "test1",
         'hpr-nummer': hprNumber,
+        'client_id': configuredClientId,
     }
     const signer = new SignJWT(fakeValidPayload)
     signer.setIssuer(issuer)
@@ -49,6 +52,28 @@ describe("FhirSession", () => {
 
         const sessionPromise = FhirSession.fromVerifiedJWT( new JwtVerificationInput(jwtTampered, fakeVerificationInput.getKey))
         await expect(sessionPromise).rejects.toThrow(FhirAuthError)
+        await expect(sessionPromise).rejects.toThrow('JWT verification failure (ERR_JWS_SIGNATURE_VERIFICATION_FAILED): signature verification failed')
+    })
+
+    test("with changed client_id claim the verification should fail", async () => {
+        const { fakeVerificationInput, issuer, hprNumber } = await prepareFakeValidVerificationInput()
+        // This is the encoded payload with client_id set to 'Some_other_app' instead of configuredClientId
+        const modifiedEncodedPayload = "eyJhdWQiOiJsZWdlZXJrbMOmcmluZyB0ZXN0Iiwic3ViIjoidGVzdDEiLCJocHItbnVtbWVyIjoiMTExMjIyIiwiaXNzIjoiaHR0cHM6Ly9hcGkuZGlwcy5uby9kaXBzLm9hdXRoIiwiY2xpZW50X2lkIjoiU29tZV9vdGhlcl9hcHAifQ"
+        // Created a jwt token where the claims part has been modified after it was signed
+        const [header, _, signature] = fakeVerificationInput.jwt.split(".")
+        const jwtTampered = `${header}.${modifiedEncodedPayload}.${signature}`
+
+        const sessionPromise = FhirSession.fromVerifiedJWT( new JwtVerificationInput(jwtTampered, fakeVerificationInput.getKey))
+        await expect(sessionPromise).rejects.toThrow(FhirAuthError)
+        await expect(sessionPromise).rejects.toThrow('JWT verification failure (ERR_JWS_SIGNATURE_VERIFICATION_FAILED): signature verification failed')
+    })
+
+    test("with wrong client_id claim the verification should fail", async () => {
+        const otherClientId = "Some_other_app"
+        const { fakeVerificationInput, issuer, hprNumber } = await prepareFakeValidVerificationInput(otherClientId)
+
+        const sessionPromise = FhirSession.fromVerifiedJWT(fakeVerificationInput)
+        expect(sessionPromise).rejects.toThrow(FhirAuthError)
     })
 
     test("with key mismatch verification should fail", async () => {
--- src/auth/fhir/FhirSession.ts
+++ src/auth/fhir/FhirSession.ts
@@ -4,6 +4,7 @@ import { JwtVerificationInput } from "@/auth/fhir/JwtVerificationInput";
 import { isIssuer, Issuer } from "@/auth/fhir/Issuer";
 import { HprNumber, isHprNumber } from "@/models/HprNumber";
 import { FhirSessionIssuers } from "@/auth/fhir/FhirSessionIssuers";
+import { fhirClientId } from "@/utils/environment";
 
 export class FhirSession {
     /**
@@ -22,21 +23,29 @@ export class FhirSession {
             issuer: FhirSessionIssuers.all,             // Verify that issuer is one of the valid ones.
             algorithms: ['RS256', 'edDSA', 'ES256'],    // Verify that approved algorithm is used.
         }
+        const expectedClientId = await fhirClientId()
         try {
             const { payload, protectedHeader } = await jose.jwtVerify(verificationInput.jwt, verificationInput.getKey, options)
-            if(isIssuer(payload.iss)) {
-                // hpr-nummer claim is probably DIPS specific. Maybe not neccessary to have either, or solvable in some other way if needed.
-                const hprNummer = payload['hpr-nummer']
-                if(hprNummer === undefined) {
-                    throw new FhirAuthError(`No hpr-nummer claim found in JWT`)
-                }
-                if(isHprNumber(hprNummer)) {
-                    return new FhirSession(payload.iss, hprNummer)
+            // To verify that the received JWT is issued for our app, and not some other, we verify that the client_id
+            // matches expected value. (The aud claim from opendips does not seem to hold any valuable info about this.)
+            const clientId = payload['client_id']
+            if(clientId === expectedClientId) {
+                if(isIssuer(payload.iss)) {
+                    // hpr-nummer claim is probably DIPS specific. Maybe not neccessary to have either, or solvable in some other way if needed.
+                    const hprNummer = payload['hpr-nummer']
+                    if(hprNummer === undefined) {
+                        throw new FhirAuthError(`No hpr-nummer claim found in JWT`)
+                    }
+                    if(isHprNumber(hprNummer)) {
+                        return new FhirSession(payload.iss, hprNummer)
+                    } else {
+                        throw new FhirAuthError(`Invalid hpr-nummer claim in JWT`)
+                    }
                 } else {
-                    throw new FhirAuthError(`Invalid hpr-nummer claim in JWT`)
+                    throw new FhirAuthError(`jwt iss ("${payload.iss}") has invalid format`)
                 }
             } else {
-                throw new FhirAuthError(`jwt iss ("${payload.iss}") has invalid format`)
+                throw new FhirAuthError(`jwt client_id ("${clientId}") does not equal "${expectedClientId}"`)
             }
         } catch (err) {
             if(err instanceof jose.errors.JOSEError) {
