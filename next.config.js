ddde0a822165a3d1599adc0ff860ef944a4dbf6d
Jostein Stuhaug <jostein.stuhaug@nav.no>
2024-01-23 13:21:56 +0100

    Fjern FHIR_CLIENT_ID frå next.config.js og.
    Sidan den var fjerna alle andre stader.

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
     output: "standalone",
     env: {
-        FHIR_CLIENT_ID: process.env.FHIR_CLIENT_ID,
         FHIR_BASE_URL: process.env.FHIR_BASE_URL,
         FHIR_SUBSCRIPTION_KEY: process.env.FHIR_SUBSCRIPTION_KEY,
         AZURE_APP_CLIENT_ID: process.env.AZURE_APP_CLIENT_ID,
