bbdd37144d69f3c58727165fa2bc6d68f6ff9cd0
Jostein Stuhaug <jostein.stuhaug@nav.no>
2024-03-12 13:53:36 +0100

    Flytt /launch inn i (withApis).
    For å teste initialisering av webmed fhir klient (uten proxying).

---
+++ src/app/(withErrorCapture)/(withApis)/launch/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export const dynamic = 'force-dynamic'
+
+export default function Page() {
+    return <>
+        <h1>Webmed launch test</h1>
+        <p>This page should redirect away quite soon, when fhir client auth is done.</p>
+    </>
+}
