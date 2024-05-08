daf66ea9d02db5cbc29bbbada035598846198bca
Leo-Andreas Ervik <6738905+leoervik@users.noreply.github.com>
2024-05-08 09:11:38 +0200

    rydd opp kode

--- src/app/api/pdf/route.ts
+++ src/app/api/pdf/route.ts
@@ -1,16 +1,15 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { logRequest } from '@/utils/loggerUtils';
-import { logger } from '@navikt/next-logger';
-import { FakeHelseopplysningerApi1 } from "@/app/simulation/fakeHelseopplysningerApi1";
+import {NextRequest, NextResponse} from 'next/server';
+import {logRequest} from '@/utils/loggerUtils';
+import {logger} from '@navikt/next-logger';
+import {FakeHelseopplysningerApi1} from "@/app/simulation/fakeHelseopplysningerApi1";
 import HelseopplysningerClient from "@/integrations/helseopplysningerserver/HelseopplysningerClient";
-import { PSBLegeerklæringInnsending } from "@/integrations/helseopplysningerserver/types/HelseopplysningerTypes";
-import { FhirSession } from "@/auth/fhir/FhirSession";
-import { JwtVerificationInput } from "@/auth/fhir/JwtVerificationInput";
-import { legeerklaeringDokumentSchema } from "@/app/components/legeerklaering/legeerklaeringDokumentSchema";
+import {FhirSession} from "@/auth/fhir/FhirSession";
+import {JwtVerificationInput} from "@/auth/fhir/JwtVerificationInput";
+import {legeerklaeringDokumentSchema} from "@/app/components/legeerklaering/legeerklaeringDokumentSchema";
 import {
     mapTilPSBLegeerklæringInnsending
 } from "@/integrations/helseopplysningerserver/types/mapTilPSBLegeerklæringInnsending";
-import { ValidationError } from "yup";
+import {ValidationError} from "yup";
 
 const initHelseopplysningerApi = () => process.env.FAKE_HELSEOPPLYSNINGER === "fake1" ?
     new FakeHelseopplysningerApi1() :
@@ -28,7 +27,7 @@ export const POST = async (request: NextRequest): Promise<Response> => {
         const legeerklæringDokument = await legeerklaeringDokumentSchema.validate(requestJson)
         const innsending = mapTilPSBLegeerklæringInnsending(legeerklæringDokument)
         // Check that hpr-number of session equals posted document
-        if(innsending.lege.hpr === fhirSession.hprNumber) {
+        if (innsending.lege.hpr === fhirSession.hprNumber) {
             // XXX Her kunne vi evt ha validering av nokre innsendte skjemaopplysninger opp mot fhir api, før registrering av
             // data i NAV sine system. Feks at lege sitt namn er korrekt, spesialiststatus, pasient-namn, etc.
             const helseopplysningerApi = initHelseopplysningerApi()
@@ -38,8 +37,8 @@ export const POST = async (request: NextRequest): Promise<Response> => {
         } else {
             return new NextResponse(`fhir session hpr-nummer ulikt legeerklæring (${fhirSession.hprNumber} != ${innsending.lege.hpr})`, {status: 400})
         }
-    } catch(e) {
-        if(e instanceof ValidationError) {
+    } catch (e) {
+        if (e instanceof ValidationError) {
             return new NextResponse(`${e.message}`, {status: 400})
         } else {
             throw e
