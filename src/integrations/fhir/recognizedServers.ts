29e0f6ca6a862f62e1f009a7a2999c7697926b86
Leo-Andreas Ervik <6738905+leoervik@users.noreply.github.com>
2024-05-14 12:02:24 +0200

    legg til Helse Vest som gjennkjennbare servere

--- src/integrations/fhir/recognizedServers.ts
+++ src/integrations/fhir/recognizedServers.ts
@@ -1,5 +1,5 @@
 export const recognizedServers = {
     OPENDIPS_TEST: "https://api.dips.no/fhir",
     WEBMED_TEST: "https://fhirapi.public.webmedepj.no/",
-    // HELSE_VEST_TEST: "https://vir-web5459.ihelse.net/fhir", TODO hent DNS, ikke statisk server
+    HELSE_VEST_TEST: "https://vir-web5459.ihelse.net/fhir",
 }
